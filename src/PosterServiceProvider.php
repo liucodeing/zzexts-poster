55d29d8b669195f10d2bc7e8bc34a0cdbc065b25
Mac <liucodeing@icloud.com>
2022-11-12 17:31:41 +0800

    注册安装命令

--- src/PosterServiceProvider.php
+++ src/PosterServiceProvider.php
@@ -7,13 +7,18 @@ use Illuminate\Support\ServiceProvider;
 
 class PosterServiceProvider extends ServiceProvider
 {
+
+    protected $commands = [
+        Console\InstallCommand::class,
+    ];
+
     /**
      * {@inheritdoc}
      */
     public function boot(Poster $extension)
     {
-        if (! Poster::boot()) {
-            return ;
+        if (!Poster::boot()) {
+            return;
         }
 
         if ($views = $extension->views()) {
@@ -22,13 +27,16 @@ class PosterServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole() && $assets = $extension->assets()) {
             $this->publishes([$assets => public_path('vendor/zzexts/poster')], 'poster');
-            $this->publishes([__DIR__.'/../config' => config_path()], 'zzexts-poster-config');
+            $this->publishes([__DIR__ . '/../config' => config_path()], 'zzexts-poster-config');
         }
 
         $this->app->booted(function () {
-            Poster::routes(__DIR__.'/../routes/web.php');
+            Poster::routes(__DIR__ . '/../routes/web.php');
         });
+    }
 
-
+    public function register()
+    {
+        $this->commands($this->commands);
     }
 }
